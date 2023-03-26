778d63bd6c10606cc9ee0c144b59017784d43cc2
tingxnb@gmail.com <tingxnb@gmail.com>
2023-03-26 15:10:19 -0400

    create transaction.py file

---
+++ pa03/Transaction.py
@@ -0,0 +1,63 @@
+'''
+Transaction.py is an Object Relational Mapping to the transactions database
+
+The ORM will work map SQL rows with the schema
+    (item #, amount, category, date, description)
+    
+    ######待补充
+
+In place of SQL queries, we will have method calls.
+
+This app will store the data in a SQLite database ~/transactions.db
+
+'''
+import sqlite3
+import os
+
+def toDict(t):
+    ''' t is a tuple (item #, amount, category, date, description)'''
+    print('t='+str(t))
+    transactions = {'item #':t[0], 'amount':t[1], 'category':t[2], 'date':t[3], 'description': t[4]}
+    return transactions
+
+class Transaction():
+    def __init__(self):
+        self.runQuery('''CREATE TABLE IF NOT EXISTS transactions
+                    (item # INTEGER PRIMARY KEY,
+                    amount INTEGER,
+                    category TEXT,
+                    date DATE,
+                    description TEXT)''',())
+
+    def selectAll(self):
+        ''' return all of the transactions as a list of dicts.'''
+        return self.runQuery("SELECT rowid,* from todo",())
+
+    def selectCompleted(self):
+        ''' return all of the completed tasks as a list of dicts.'''
+        return self.runQuery("SELECT rowid,* from todo where completed=1",())
+
+    def add(self,item):
+        ''' create a todo item and add it to the todo table '''
+        return self.runQuery("INSERT INTO todo VALUES(?,?,?)",(item['title'],item['desc'],item['completed']))
+
+    def delete(self,rowid):
+        ''' delete a todo item '''
+        return self.runQuery("DELETE FROM todo WHERE rowid=(?)",(rowid,))
+
+    def setComplete(self,rowid):
+        ''' mark a todo item as completed '''
+        return self.runQuery("UPDATE todo SET completed=1 WHERE rowid=(?)",(rowid,))
+
+    def runQuery(self,query,tuple):
+        ''' return all of the transaction as a list of dicts.'''
+        con= sqlite3.connect(os.getenv('HOME')+'/transaction.db')
+        cur = con.cursor() 
+        cur.execute(query,tuple)
+        tuples = cur.fetchall()
+        con.commit()
+        con.close()
+        return [toDict(t) for t in tuples]
+
+    
+        
