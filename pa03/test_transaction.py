0310594bc49ceba2da8341adba91ba053bd0407f
tingxnb@gmail.com <tingxnb@gmail.com>
2023-03-26 21:26:09 -0400

    add test of three category method

--- pa03/test_transaction.py
+++ pa03/test_transaction.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 from Transaction import Transaction
 
 def test_selectYearMonthDate():
@@ -23,5 +24,35 @@ def test_selectYearMonthDate():
     assert t3[0]['amount'] == item1['amount']
     assert t4[0]['category'] == item2['category']
     transaction.runQuery("DELETE FROM transactions", ())
+
+def test_add_category():
+    test_db = 'test_category.db'
+    transaction = Transaction(test_db) 
+    transaction.add_categories(None, 'test', None, None)
+    categories = transaction.show_categories()
+    assert len(categories) == 1
+    assert categories[0] == 'test'
+
+def test_show_categories():
+    test_db = 'test_show_category.db'
+    transaction = Transaction(test_db) 
+    item1 = {'amount': 50, 'category': 'Food', 'date': '2023-03-26', 'description': 'Delicious Food'}
+    item2 = {'amount': 100, 'category': 'Gas', 'date': '2023-02-28', 'description': 'Filled up gas tank'}
+    transaction.add_transaction(item1)
+    transaction.add_transaction(item2)
+    categories = transaction.show_categories()
+    assert len(categories) == 2
+    assert 'Food' in categories
+    assert 'Gas' in categories
+    
+def test_modify_category():
+    test_db = 'test_modify_category.db'
+    transaction = Transaction(test_db) 
+    item1 = {'amount': 50, 'category': 'Food', 'date': '2023-03-26', 'description': 'Delicious Food'}
+    transaction.add_transaction(item1)
+    transaction.modify_categories('Food', 'test')
+    categories = transaction.show_categories()
+    assert categories[0] == 'test'
+    
 
 
